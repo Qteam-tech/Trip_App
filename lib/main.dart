15ab88eb2f00f5a777f0f4c49fa153789ebcb3f9
M.Keshtehgar <mk.qeshmair@gmail.com>
2022-06-07 10:18:56 +0430

    delete the sandbox

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Trip App',
-      home: Sandbox(),
+      home: Home(),
     );
   }
 }
